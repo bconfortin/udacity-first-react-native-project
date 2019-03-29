3546067dab6c7cf21a2897ff616662e251e2b154
Bruno Goerck Confortin <brunoconfortin@gmail.com>
2019-03-29 14:10:55 -0300

    Feature: added slider and stepper functionality, still no style

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Slider} from 'react-native'
 
-export default function UdaciSlider () {
+export default function UdaciSlider ({max, unit, step, value, onChange}) {
     return (
         <View>
             <Text>UdaciSlider</Text>
+            <Slider
+                maximumValue={max}
+                minimumValue={0}
+                unit={unit}
+                step={step}
+                value={value}
+                onValueChange={onChange}/>
+            <Text>{value}</Text>
+            <Text>{unit}</Text>
         </View>
     )
 }
